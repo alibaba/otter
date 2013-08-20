9310fa024f755ccae3100adf2682806a7f2b7456
agapple <jianghang115@gmail.com>
2013-08-20 22:48:46 +0800

    fixed issue #4 修改debug端口为7099,8099

--- node/deployer/src/main/bin/startup.bat
+++ node/deployer/src/main/bin/startup.bat
@@ -16,7 +16,7 @@ set CLASSPATH=%conf_dir%\..\lib\*;%CLASSPATH%
 
 set JAVA_MEM_OPTS= -Xms128m -Xmx512m -XX:PermSize=128m
 set JAVA_OPTS_EXT= -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dapplication.codeset=UTF-8 -Dfile.encoding=UTF-8
-set JAVA_DEBUG_OPT= -server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=9099,server=y,suspend=n
+set JAVA_DEBUG_OPT= -server -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8099,server=y,suspend=n
 set OTTER_OPTS= -DappName=otter-node -Dlogback.configurationFile="%logback_configurationFile%" -Dnid=%nid%
 
 set JAVA_OPTS= %JAVA_MEM_OPTS% %JAVA_OPTS_EXT% %JAVA_DEBUG_OPT% %OTTER_OPTS%
