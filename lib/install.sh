8a389fc967311ed103fa79066be03cd600ca64d7
agapple <jianghang115@gmail.com>
2013-08-22 11:53:06 +0800

    增加jtester本地lib库

--- lib/install.sh
+++ lib/install.sh
@@ -1,1 +1,2 @@
 mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.3.0 -Dpackaging=jar -Dfile=ojdbc14-10.2.0.3.0.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.jtester -DartifactId=jtester -Dversion=1.1.8 -Dpackaging=jar -DpomFile=jtester-1.1.8.pom -Dfile=jtester-1.1.8.jar -Dsources=jtester-1.1.8-sources.jar
