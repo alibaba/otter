c48d4ac0abea5ef49d5495c79fa4f112c581fadb
agapple <jianghang115@gmail.com>
2013-09-02 17:48:40 +0800

    增加文件同步示例

--- node/extend/src/main/java/com/alibaba/otter/node/extend/fileresolver/TestFileResolver.java
+++ node/extend/src/main/java/com/alibaba/otter/node/extend/fileresolver/TestFileResolver.java
@@ -18,21 +18,21 @@ package com.alibaba.otter.node.extend.fileresolver;
 
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.alibaba.otter.shared.etl.extend.fileresolver.FileInfo;
 
 public class TestFileResolver extends AbstractFileResolver {
 
     public FileInfo[] getFileInfo(Map<String, String> rowMap) {
-        String labelAddress = rowMap.get("REMARKS");
-        String arandaAddress = rowMap.get("REMARKS2");
+        // 基本步骤：
+        // 1. 获取binlog中的变更字段，比如组成文件有多个字段组成version+path
+        // 2. 基于字段内容，构造一个文件路径，目前开源版本只支持本地文件的同步.(如果是网络文件，建议进行NFS mount到ndde机器).
+        // 3. 返回FileInfo数组，(目前不支持目录同步，如果是目录需要展开为多个FileInfo的子文件)，如果不需要同步，则返回null.
+        String path = rowMap.get("FIELD"); //注意为大写
         FileInfo fileInfo = null;
-        if (labelAddress != null && labelAddress.length() != 0) {
-            if (arandaAddress != null && arandaAddress.length() != 0) {
-                fileInfo = new FileInfo(arandaAddress, labelAddress);
-            } else {
-                fileInfo = new FileInfo(labelAddress);
-            }
-
+        if (StringUtils.isNotEmpty(path)) {
+            fileInfo = new FileInfo(path);
             return new FileInfo[] { fileInfo };
         } else {
             return null;
