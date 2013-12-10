c2230655d0dca34172a28198644846ea992f4def
rdongxie <rdongxie@gmail.com>
2013-12-10 11:53:10 +0800

    add ojdbc14-10.2.0.3.0.jar&jtester-1.1.8.jar install script:install.bat

---
+++ lib/install.bat
@@ -0,0 +1,2 @@
+call mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.3.0 -Dpackaging=jar -Dfile=ojdbc14-10.2.0.3.0.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.jtester -DartifactId=jtester -Dversion=1.1.8 -Dpackaging=jar -DpomFile=jtester-1.1.8.pom -Dfile=jtester-1.1.8.jar -Dsources=jtester-1.1.8-sources.jar
