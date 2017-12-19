002bf9806578bba299d5fda52b55c314a73b3618
LYC <liyongcun120@163.com>
2017-12-19 17:29:38 +0800

    where条件的过滤

--- node/extend/src/main/java/com/alibaba/otter/node/extend/processor/TradOrder_Processor.java
+++ node/extend/src/main/java/com/alibaba/otter/node/extend/processor/TradOrder_Processor.java
@@ -9,17 +9,14 @@ import java.util.List;
 
 public class TradOrder_Processor extends AbstractEventProcessor {
     public boolean process(EventData eventData) {
-        // 基本步骤：
-
-        // 构造新的主键,老的主键uid插入普通值中
         Boolean ck_flag=false;
         for (EventColumn column : eventData.getKeys()) {
-            if (column.getColumnName().toLowerCase().equals("outer_pay_status")){
+            if (column.getColumnName().toLowerCase().toLowerCase().equals("outer_pay_status")){
                  if (column.getColumnValue().equals("1")) {
                      ck_flag = true;
                  }
             }
-            if (column.getColumnName().toLowerCase().equals("pay_user_id")){
+            if (column.getColumnName().toLowerCase().toLowerCase().equals("pay_user_id")){
                 if (column.isNull() || column.getColumnValue().length() <=0){
                     ck_flag=ck_flag&&true;
                 } else
