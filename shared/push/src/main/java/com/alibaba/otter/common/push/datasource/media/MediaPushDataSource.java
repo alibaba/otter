2727d65231b92fa03b5fa51724d8df4874c81430
七锋 <jianghang.loujh@alibaba-inc.com>
2018-09-18 10:21:52 +0800

    fixed issue #607, support media dataSource utf8mb4

--- shared/push/src/main/java/com/alibaba/otter/common/push/datasource/media/MediaPushDataSource.java
+++ shared/push/src/main/java/com/alibaba/otter/common/push/datasource/media/MediaPushDataSource.java
@@ -22,6 +22,7 @@ import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
 
 import javax.sql.CommonDataSource;
 import javax.sql.DataSource;
@@ -160,8 +161,15 @@ public class MediaPushDataSource implements DataSource {
             dbcpDs.addConnectionProperty("rewriteBatchedStatements", "true");
             dbcpDs.addConnectionProperty("zeroDateTimeBehavior", "convertToNull");// 将0000-00-00的时间类型返回null
             dbcpDs.addConnectionProperty("yearIsDateType", "false");// 直接返回字符串，不做year转换date处理
+            dbcpDs.addConnectionProperty("noDatetimeStringSync", "true");// 返回时间类型的字符串,不做时区处理
+            dbcpDs.addConnectionProperty("jdbcCompliantTruncation", "false");// 允许sqlMode为非严格模式
             if (StringUtils.isNotEmpty(encoding)) {
-                dbcpDs.addConnectionProperty("characterEncoding", encoding);
+                if (StringUtils.equalsIgnoreCase(encoding, "utf8mb4")) {
+                    dbcpDs.addConnectionProperty("characterEncoding", "utf8");
+                    dbcpDs.setConnectionInitSqls(Arrays.asList("set names utf8mb4"));
+                } else {
+                    dbcpDs.addConnectionProperty("characterEncoding", encoding);
+                }
             }
             dbcpDs.setValidationQuery("select 1");
         } else {
