1565ef87e4f71bf6afa9723923d4cfe52296b291
codebee <codebee@sina.com>
2018-11-28 12:06:53 +0800

    去掉dockerfile中RUN命令中的空行，后面版本的docker可能会把它作为错误对待

--- lib/install.sh
+++ lib/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.1.0.7.0 -Dpackaging=jar -Dfile=ojdbc6.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.jtester -DartifactId=jtester -Dversion=1.1.8 -Dpackaging=jar -DpomFile=jtester-1.1.8.pom -Dfile=jtester-1.1.8.jar -Dsources=jtester-1.1.8-sources.jar
 mvn install:install-file -DgroupId=mockit -DartifactId=jmockit -Dversion=0.999.10 -Dpackaging=jar -DpomFile=jmockit-0.999.10.pom -Dfile=jmockit-0.999.10.jar -Dsources=jmockit-0.999.10-sources.jar
